1c21eb8b8f55527cfa93088b2ba28b960c781b90
euno <seal5945@gmail.com>
2022-09-08 19:35:41 +0900

    image

--- instargram/models.py
+++ instargram/models.py
@@ -3,6 +3,12 @@ from django.db import models
 
 
 class Post(models.Model):
-    content = models.TextField()  # 내용
+    message = models.TextField()  # 내용.
+    photo = models.ImageField(blank=True, upload_to='instargram/post/%Y/%m/%d')
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)  # 데이터를 추가한 시간
     updated_at = models.DateTimeField(auto_now=True)  # 현재 시간
+
+    def __str__(self):
+        return self.message
+
