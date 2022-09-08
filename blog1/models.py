bc69e47e148a83876838a2f0e08552a78d4b1f95
euno <seal5945@gmail.com>
2022-09-08 14:19:49 +0900

    first commit

---
+++ blog1/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)  # 제목
+    content = models.TextField()  # 내용
+    created_at = models.DateTimeField(auto_now_add=True)  # 데이터를 추가한 시간
+    updated_at = models.DateTimeField(auto_now=True)  # 현재 시간
